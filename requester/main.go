2cabe61c07355886682e03884a5e1ecc3c08cac6
Guy <guywilks@guys-mbp-2.attlocal.net>
2019-07-12 16:38:39 -0700

    Added auto ping for nodes

--- requester/main.go
+++ requester/main.go
@@ -1,49 +1,103 @@
 package main
 
-import (
-	"fmt"
-	"context"
-	"log"
+import(
+    "context"
+    "fmt"
 	"net/http"
-	"io/ioutil"
-	//"github.com/ghodss/yaml"
 	"github.com/ericchiang/k8s"
-    corev1 "github.com/ericchiang/k8s/apis/core/v1"
+	"encoding/json"
+	corev1 "github.com/ericchiang/k8s/apis/core/v1"
+	"time"
+	"io/ioutil"
 )
+
+type Endpoint struct {
+	APIVersion string `json:"apiVersion"`
+	Kind       string `json:"kind"`
+	Metadata   struct {
+		Annotations struct {
+		} `json:"annotations"`
+		Name      string `json:"name"`
+		Namespace string `json:"namespace"`
+	} `json:"metadata"`
+	Subsets []struct {
+		Addresses []struct {
+			IP string `json:"ip"`
+		} `json:"addresses"`
+		Ports []struct {
+			Name string `json:"name"`
+			Port int    `json:"port"`
+		} `json:"ports"`
+	} `json:"subsets"`
+}
+
+type Gen struct {
+	Metadata struct {
+		Name              string    `json:"name"`
+		GenerateName      string    `json:"generateName"`
+		Namespace         string    `json:"namespace"`
+		SelfLink          string    `json:"selfLink"`
+		UID               string    `json:"uid"`
+		ResourceVersion   string    `json:"resourceVersion"`
+		Generation        int       `json:"generation"`
+		CreationTimestamp time.Time `json:"creationTimestamp"`
+		Annotations       struct {
+			KubectlKubernetesIoLastAppliedConfiguration string `json:"kubectl.kubernetes.io/last-applied-configuration"`
+		} `json:"annotations"`
+		ClusterName string `json:"clusterName"`
+	} `json:"metadata"`
+	Subsets []struct {
+		Addresses []struct {
+			IP        string `json:"ip"`
+			Hostname  string `json:"hostname"`
+			NodeName  string `json:"nodeName"`
+			TargetRef struct {
+				Kind            string `json:"kind"`
+				Namespace       string `json:"namespace"`
+				Name            string `json:"name"`
+				UID             string `json:"uid"`
+				APIVersion      string `json:"apiVersion"`
+				ResourceVersion string `json:"resourceVersion"`
+				FieldPath       string `json:"fieldPath"`
+			} `json:"targetRef"`
+		} `json:"addresses"`
+		Ports []struct {
+			Name     string `json:"name"`
+			Port     int    `json:"port"`
+			Protocol string `json:"protocol"`
+		} `json:"ports"`
+	} `json:"subsets"`
+}
 
 func main(){
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		setupResponse(&w, r)
-		if (*r).Method == "OPTIONS" {
-			return
-		}	
-		resp, _ := http.Get("http://10.0.1.53:80")
-		w.WriteHeader(200)
+		client, err := k8s.NewInClusterClient()
+		if err != nil {
+			fmt.Println(err)
+		}
+		var endpoints corev1.Endpoints
+		err = client.Get(context.Background(), "default", "counter", &endpoints)
+		if err != nil {
+			fmt.Println(err)
+		}
+		out, err := json.Marshal(endpoints)
+		if err != nil {
+			fmt.Println(err)
+		}
+		var gen Gen
+		json.Unmarshal(out, &gen)
+		if err != nil {
+			fmt.Println(err)
+		}
+		//defer fmt.Println(body)
+		//fmt.Fprintf(w, string(gen.Subsets[0].Addresses[0].IP))
+		resp, err := http.Get("http://" + gen.Subsets[0].Addresses[0].IP)
 		body, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
 			panic(err)
 		}
-		fmt.Fprint(w, string(body))
+		fmt.Fprintf(w, string(body))
 	})
+
 	http.ListenAndServe(":80", nil)
-}
-
-func getIP() string{
-	client, err := k8s.NewInClusterClient()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var endpoints corev1.Endpoints
-	if err := client.Get(context.Background(), "backend", "hello", &endpoints); err != nil {
-		log.Fatal(err)
-	}
-	callable := "http://" + (*endpoints.Subsets[0].Addresses[0].Ip)
-	return callable
-}
-
-func setupResponse(w *http.ResponseWriter, req *http.Request) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-    (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-    (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
