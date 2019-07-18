78ca218df0c95888cc26ecc75b69c580a79bda85
tester <tester@testers-MacBook-Pro.local>
2019-07-18 13:04:34 -0700

    added auto use files to nake set up and tear down easier
    Note: the set up and tear down do not ork unless the paths for the folders and application are correct

--- terraformEKS/outputs.tf
+++ terraformEKS/outputs.tf
@@ -5,3 +5,14 @@ output "kubeconfig" {
 output "config-map" {
   value = "${module.eks.config-map-aws-auth}"
 }
+
+resource "local_file" "cluster" {
+    content     = "${module.eks.kubeconfig}"
+    filename = "config-files/config"
+}
+
+resource "local_file" "nodes" {
+    content     = "${module.eks.config-map-aws-auth}"
+    filename = "config-files/nodes.yaml"
+}
+
